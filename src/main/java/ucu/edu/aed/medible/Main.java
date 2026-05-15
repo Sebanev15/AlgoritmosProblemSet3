138e5622bd1211969fcaef54dbcb15699c9f877b
Patricio <PATRICIO.BARRON@correo.ucu.edu.uy>
2026-05-15 16:38:40 -0300

    Implemento medibles

--- src/main/java/ucu/edu/aed/medible/Main.java
+++ src/main/java/ucu/edu/aed/medible/Main.java
@@ -3,7 +3,10 @@ package ucu.edu.aed.medible;
 
 import ucu.edu.aed.medible.lib.Medible;
 import ucu.edu.aed.medible.lib.Medicion;
+import ucu.edu.aed.medible.medibles.MedicionBuscarArrayList;
+import ucu.edu.aed.medible.medibles.MedicionBuscarHashMap;
 import ucu.edu.aed.medible.medibles.MedicionBuscarLinkedList;
+import ucu.edu.aed.medible.medibles.MedicionBuscarTreeMap;
 import ucu.edu.aed.tda.trie.TTrie;
 //import ucu.edu.aed.tda.trie.impl.Trie;
 import ucu.edu.aed.utils.FileUtils;
@@ -12,7 +15,7 @@ import java.util.*;
 
 public class Main {
 
-    private static final int REPETICIONES = 100;
+    private static final int REPETICIONES = 20;
 
     public static void main(String[] args) {
         //TTrie<String> trie = new Trie<>();
@@ -28,31 +31,24 @@ public class Main {
 
         for (String p : palabrasParaAgregar) {
             // insertar la palabra p en el trie
-            // TODO
+            // TODO 
             // insertar la palabra p en el linkedList
             linkedList.add(p);
             // insertar la palabra p en el arrayList
-//            arrayList.add(p);
+            arrayList.add(p);
             // insertar la palabra p en el hashMap
-            // TODO
+            hashMap.put(p, p);
             // insertar la palabra p en el treeMap
-            // TODO
+            treeMap.put(p, p);
         }
 
         List<Medible<List<String>>> medibles = new LinkedList<>();
         medibles.add(new MedicionBuscarLinkedList(linkedList));
-
-        // TODO implementar MedicionBuscarArrayList
-//        medibles.add(new MedicionBuscarArrayList(arrayList));
-
+        medibles.add(new MedicionBuscarArrayList(arrayList));
         // TODO implementar MedicionBuscarTrie
         // medibles.add(new MedicionBuscarTrie(trie));
-
-        // TODO implementar MedicionBuscarHashMap
-        // medibles.add(new MedicionBuscarHashMap(hashMap));
-
-        // TODO implementar MedicionBuscarTreeMap
-        // medibles.add(new MedicionBuscarTreeMap(treeMap));
+        medibles.add(new MedicionBuscarHashMap(hashMap));
+        medibles.add(new MedicionBuscarTreeMap(treeMap));
 
         StringBuilder sb = new StringBuilder();
         sb.append("algoritmo,tiempo,memoria\n");
